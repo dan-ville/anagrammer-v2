ca532f98c7b42d3560d3ca2595a36bef91d038d4
Daniel Villegas <d.villegas@visiontravel.net>
2022-04-10 21:43:49 -0400

    create welcome page content

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,17 +1,21 @@
-import { Button } from "@nextui-org/react"
-import React from "react"
+import { Button, Spacer, Text } from "@nextui-org/react"
 import { Link } from "react-router-dom"
+import Icon from "../../assets/Icon/Icon"
+import anagrammer from "../../img/anagrammer.svg"
 
 const Welcome = () => {
   return (
-    <>
-      <div>Welcome to Anagrammer!</div>
+    <div style={{ display: "grid", placeItems: "center", textAlign: "center" }}>
+      <Text h2>Welcome to Anagrammer!</Text>
+      <Spacer y={1} />
+      <Icon src={anagrammer} width={150} />
+      <Spacer y={2} />
       <Link to="/create">
-        <Button size="sm" auto rounded color="gradient">
-          Create some anagrams
+        <Button size="xl" auto color="gradient">
+          Let's make anagrams
         </Button>
       </Link>
-    </>
+    </div>
   )
 }
 
